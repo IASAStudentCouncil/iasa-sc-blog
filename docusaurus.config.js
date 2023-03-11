2d3f9ea247c0c3ba3f86be90e4516c8c1508bf30
prometheusua <PrometheusUA@i.ua>
2023-03-11 12:54:34 +0200

    Added build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,16 +10,11 @@ const config = {
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  url: 'https://IASAStudentCouncil.github.io/',
+  baseUrl: '/iasa-sc-blog',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'IASAStudentCouncil',
+  projectName: 'iasa-sc-blog', 
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -28,8 +23,8 @@ const config = {
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'uk',
+    locales: ['uk'],
   },
 
   presets: [
