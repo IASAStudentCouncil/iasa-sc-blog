c68c0fd9361bf6f45b31bd0566f223bc7c439d57
DLutsyuk <78265212+DimaL-cloud@users.noreply.github.com>
2024-11-21 14:00:45 +0200

    ART-5: change navbar colors combination

--- src/components/MobileTOC/MobileTOC.jsx
+++ src/components/MobileTOC/MobileTOC.jsx
@@ -1,51 +1,53 @@
 import React from "react";
-import { useState } from "react";
-import { motion } from "framer-motion";
+import {useState} from "react";
+import {motion} from "framer-motion";
 import "./MobileTOC.css";
 
 export default function MobileTOC({sections}) {
 
-  const[isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-  function handleChangeIsOpen() {
-    setIsOpen(prev => !prev);
-   }
+    function handleChangeIsOpen() {
+        setIsOpen(prev => !prev);
+    }
 
-   function toRef(heading) 
-   {
-    const ref = heading.replace(/[.,…’\/#!$%\^&\*;:{}=\_`'~—()+?«»🗓💪😎🕊⭐🛠]/g, '').replace(/\s/g, '-').toLowerCase();
-    return `#${ref}`;
-   }
-  
-  return (
-    <div className="mobile-nav">
-        <button className={`mobile-nav-toggle ${isOpen ? "active" : ""}`} onClick={handleChangeIsOpen}> На цій сторінці </button> 
-        <motion.div 
-        className='mobile-nav-content'
-        initial={{ height: 0 }}
-        animate={{ height: isOpen ? "auto" : 0 }}
-        transition={{ duration: 0.4, ease: "easeInOut" }}> 
-          <ul className="mobile-nav-list">
-          { sections.map((section, sectionIndex) =>
-          { return (
-          <li key={sectionIndex}> 
-              <a href={toRef(section.heading)}> {section.heading}</a>
-              {section.subHeadings && section.subHeadings.length > 0 && (
-              <ul>
-                {
-                  section.subHeadings.map((subHeading, subHeadingIndex) => {
-                    return (
-                      <li key={`${sectionIndex}.${subHeadingIndex}`}> <a href={toRef(subHeading)}> {subHeading} </a> </li>
-                    )
-                  })
-                }
-              </ul>
-          )}
-          </li>
-          )
-          }) }
-          </ul> 
-        </motion.div>
-    </div>
-  )
+    function toRef(heading) {
+        const ref = heading.replace(/[.,…’\/#!$%\^&\*;:{}=\_`'~—()+?«»🗓💪😎🕊⭐🛠]/g, '').replace(/\s/g, '-').toLowerCase();
+        return `#${ref}`;
+    }
+
+    return (
+        <div className="mobile-nav">
+            <button className={`mobile-nav-toggle ${isOpen ? "active" : ""}`} onClick={handleChangeIsOpen}> На цій
+                сторінці
+            </button>
+            <motion.div
+                className='mobile-nav-content'
+                initial={{height: 0}}
+                animate={{height: isOpen ? "auto" : 0}}
+                transition={{duration: 0.4, ease: "easeInOut"}}>
+                <ul className="mobile-nav-list">
+                    {sections.map((section, sectionIndex) => {
+                        return (
+                            <li key={sectionIndex}>
+                                <a href={toRef(section.heading)}> {section.heading}</a>
+                                {section.subHeadings && section.subHeadings.length > 0 && (
+                                    <ul>
+                                        {
+                                            section.subHeadings.map((subHeading, subHeadingIndex) => {
+                                                return (
+                                                    <li key={`${sectionIndex}.${subHeadingIndex}`}><a
+                                                        href={toRef(subHeading)}> {subHeading} </a></li>
+                                                )
+                                            })
+                                        }
+                                    </ul>
+                                )}
+                            </li>
+                        )
+                    })}
+                </ul>
+            </motion.div>
+        </div>
+    )
 }
